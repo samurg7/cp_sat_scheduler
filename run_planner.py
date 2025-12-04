68622299f4c0293470e908fef226c0c763a9398d
samuel rodas <samuelrodasgonzalez@gmail.com>
2025-12-03 23:10:32 -0500

    feat: archivo run que permite ejecutar el proyecto desde la consola

--- run_planner.py
+++ run_planner.py
@@ -1,1 +1,17 @@
+from datetime import date
+from scheduler import Scheduler
+
+if __name__ == "__main__":
+    advisors = ["A1", "A2", "A3"]
+
+    s = Scheduler(
+        advisors=advisors,
+        start_date=date.today(),
+        holidays=[]
+    )
+
+    result = s.solve()
+    print(result["pivot"])
+    result["pivot"].to_csv("resultado.csv", index=False)
+    print("Archivo resultado.csv generado.")
 
